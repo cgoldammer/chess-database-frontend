1e45b1bc0ad1a9ceaa5b4ed7f07eff09ed6f50dd
cgoldammer <goldammer.christian@gmail.com>
2017-04-08 15:40:00 +0000

    Updated app

--- js/app.jsx
+++ js/app.jsx
@@ -4,51 +4,53 @@ import { Button } from 'react-bootstrap';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import Chessdiagram from 'react-chessdiagram';
-import myData from '../mates.json';
+import myData from '/home/cg/data/output/mates.json';
 
 String.prototype.number = function (find) {
-	return this.split(find).length - 1;
+  return this.split(find).length - 1;
 }
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			selectedIndex: 0,
-			showSolution: false
-		};
-		this._selectRowProp = this._selectRowProp.bind(this);
-		this._onShowSolutionClicked = this._onShowSolutionClicked.bind(this);
-	}
-	_onRowSelect(row, isSelected, e){
-		console.log(row);
-		this.setState({selectedIndex: row.id, showSolution: false});
-		console.log(row.best);
-	}
-	_selectRowProp() {
-		return {
-			mode: 'radio',
-			clickToSelect: true,
-			onSelect: this._onRowSelect.bind(this),
-			bgColor: 'pink',
-			hideSelectColumn: true
-		}
-	}
-	_onShowSolutionClicked(){
-		this.setState((prevState, props) => ({showSolution: !prevState.showSolution}));
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedIndex: 0,
+      showSolution: false
+    };
+    this._selectRowProp = this._selectRowProp.bind(this);
+    this._onShowSolutionClicked = this._onShowSolutionClicked.bind(this);
+  }
+  _onRowSelect(row, isSelected, e){
+    console.log(row);
+    this.setState({selectedIndex: row.id, showSolution: false});
+    console.log(row.best);
+  }
+  _selectRowProp() {
+    return {
+      mode: 'radio',
+      clickToSelect: true,
+      onSelect: this._onRowSelect.bind(this),
+      bgColor: 'pink',
+      hideSelectColumn: true
+    }
+  }
+  _onShowSolutionClicked(){
+    this.setState((prevState, props) => ({showSolution: !prevState.showSolution}));
+  }
   render() {
     return (
       <Grid fluid>
         <Row>
-          <Col xs={6} md={6}>
-						<PositionTable selectedIndex={this.state.selectedIndex} selectRow={this._selectRowProp()}/>
+          <Col md={6}>
+                <PositionTable selectedIndex={this.state.selectedIndex} selectRow={this._selectRowProp()}/>
           </Col>
-          <Col xs={3} md={3}>
-						<Row>
-							<Board selectedIndex={this.state.selectedIndex}/>
-							<SolutionShow showSolution={this.state.showSolution} selectedIndex={this.state.selectedIndex} onClick={this._onShowSolutionClicked}/>
-						</Row>
+          <Col md={6}>
+            <Row>
+              <Board selectedIndex={this.state.selectedIndex}/>
+            </Row>
+            <Row>
+              <SolutionShow showSolution={this.state.showSolution} selectedIndex={this.state.selectedIndex} onClick={this._onShowSolutionClicked}/>
+            </Row>
           </Col>
         </Row>
       </Grid>
@@ -57,64 +59,67 @@ class App extends React.Component {
 }
 
 class SolutionShow extends React.Component {
-	constructor(props){
-		super(props);
-	}
-	render () {
-		return (
-			<div class={"show-solution"} onClick={this.props.onClick}>{this.props.showSolution ? "Solution is: " + myData[this.props.selectedIndex].best : "Show solution"}</div>
-		)
-	}
+  constructor(props){
+    super(props);
+  }
+  render () {
+    var style = {
+      fontSize: 30
+    };
+    return (
+      <div onClick={this.props.onClick} style={style}>{this.props.showSolution ? "Solution is: " + myData[this.props.selectedIndex].best : "Show solution"}</div>
+    )
+  }
 }
 
 function fenName(fen, row){
-	const pieces = ["q", "r", "b", "k"]
-	var str = "White:"
-	for (let piece of pieces){
-		str += " " + piece.toUpperCase() + fen.number(piece.toUpperCase());
-	}
-	str += " | Black:"
-	for (let piece of pieces){
-		str += " " + piece.toUpperCase() + fen.number(piece);
-	}
-	return str;
+  const pieces = ["q", "r", "b", "k"]
+  var str = "White:"
+  for (let piece of pieces){
+    str += " " + piece.toUpperCase() + fen.number(piece.toUpperCase());
+  }
+  str += " | Black:"
+  for (let piece of pieces){
+    str += " " + piece.toUpperCase() + fen.number(piece);
+  }
+  return str;
 }
 
 
 class PositionTable extends React.Component {
-	constructor(props){
-		console.log(props.selectRow);
-		super(props);
-	}
-	render() {
-		const selectRow = {mode:'radio'};
-		return (
-			<BootstrapTable data={ myData } selectRow={ this.props.selectRow }>
-				<TableHeaderColumn dataField='id' isKey>Id</TableHeaderColumn>
-				<TableHeaderColumn dataField='fen' dataFormat={fenName} width='50%'>Position type</TableHeaderColumn>
-				<TableHeaderColumn dataField='mate' width='10%'>Best Mate</TableHeaderColumn>
-			</BootstrapTable>)
-	}
+  constructor(props){
+    console.log(props.selectRow);
+    super(props);
+  }
+  render() {
+    const selectRow = {mode:'radio'};
+    return (
+      <BootstrapTable data={ myData } selectRow={ this.props.selectRow }>
+        <TableHeaderColumn dataField='id' isKey>Id</TableHeaderColumn>
+        <TableHeaderColumn dataField='fen' dataFormat={fenName} width='50%'>Position type</TableHeaderColumn>
+        <TableHeaderColumn dataField='mate' width='10%'>Best Mate</TableHeaderColumn>
+      </BootstrapTable>)
+  }
 }
 
 const squareSize = 70;
 
 class Board extends React.Component {
-	constructor(props){
-		props.lightSquareColor = props.lightSquareColor || "#2492FF" // light blue
-		props.darkSquareColor = props.darkSquareColor || "#005EBB" // dark blue
-		props.flip = props.flip || false
-		super(props);
-	}
-	render() {
-		return (
-			<Chessdiagram 
-				flip={this.props.flip} 
-				squareSize={squareSize} 
-				lightSquareColor={this.props.lightSquareColor} 
-				darkSquareColor={this.props.darkSquareColor} 
-				fen={myData[this.props.selectedIndex].fen.substring(4)}/>)
-	}
+  constructor(props){
+    props.lightSquareColor = props.lightSquareColor || "#2492FF" // light blue
+    props.darkSquareColor = props.darkSquareColor || "#005EBB" // dark blue
+    props.flip = props.flip || false
+    super(props);
+  }
+  render() {
+    return (
+      <Chessdiagram 
+        flip={this.props.flip} 
+        squareSize={squareSize} 
+        lightSquareColor={this.props.lightSquareColor} 
+        darkSquareColor={this.props.darkSquareColor} 
+        fen={myData[this.props.selectedIndex].fen.substring(4)}/>)
+  }
 }
 
 
